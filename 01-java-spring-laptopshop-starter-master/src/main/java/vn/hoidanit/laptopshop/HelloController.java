667ea51b881302f286bf42210882aaded548f152
shyhubb <shyhubel@gmai.com>
2025-02-26 19:57:10 +0700

    comitfix

--- 01-java-spring-laptopshop-starter-master/src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ 01-java-spring-laptopshop-starter-master/src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "shyhub come to Spring boot";
+        return "shyhub come to Spring boot hehe";
     }
 }
